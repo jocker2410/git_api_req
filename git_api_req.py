33e604a27582a2389657d736bd349648b2bdd937
jocker2410 <22291909+jocker2410@users.noreply.github.com>
2024-03-22 17:50:30 +0100

    Update git_api_req.py
    unnecessary elements removed

--- git_api_req.py
+++ git_api_req.py
@@ -1,13 +1,11 @@
 from colorama import Fore, Style
-from datetime import datetime
-import requests as rqq
-from pprint import pprint
-import os, sys
+import requests, datetime, os, sys
 
-now = datetime.now()
+
+now = datetime.datetime.now()
 Y = now.strftime("%Y")
 M = now.strftime("%M")
-
+# If the first argument is not included, then CVE and current year should be used as the search word
 arg = sys.argv[1] if len(sys.argv) > 1 else f"cve-{Y}"
 
 uri = 'https://api.github.com/search/repositories'
@@ -15,7 +13,7 @@ params = {'q': arg}
 os.system("clear")
 os.system("cls")
 print(Fore.GREEN + "[+] Start Connection")
-resp = rqq.get(uri, params=params)
+resp = requests.get(uri, params=params)
 js_resp = resp.json()
 
 print(Fore.YELLOW + "[+] Sort items")
@@ -23,15 +21,12 @@ total_num = js_resp['total_count']
 items = js_resp['items']
 sorted_items = sorted(items, key=lambda x: x['html_url'].split('/')[4].lower())
 print(Style.RESET_ALL)
-n = 0
-for item in sorted_items:
-  n = n+1
+for item in sorted_items: 
   y = item['pushed_at'].split('-')[0]
   m = item['pushed_at'].split('-')[1]
   last_update = item['pushed_at']
-
+  
   if (y == Y ):
     url = item['html_url']
     print(f"Last Update: {last_update}\nUrl:\n{url}\n")
-    #print(item['pushed_at'].split('-')[1])
-print(Fore.GREEN + f"Total: {total_num}\nSows: {n}")
+print(Fore.GREEN + f"Total: {total_num}")
