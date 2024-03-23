6bbdd983606dbfb5ecdd0d4516f9d27b5a769724
jocker2410 <jocker2410@gmail.com>
2024-03-23 23:55:54 +0100

    readme and script updated

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Github API Request
-A simple API tester that connects to the Github AI to collect information.
+# GitHub API Request Script ?
+
+This script was created to retrieve and display information from `api.github.com`. It's a simple yet powerful tool that helps you find the latest CVE repositories from the current year (CVE-(Current Year)). ?
+
+## How it works ??
+
+You can simply start the script and get the repository results from the current year. Or you can start it with a parameter, such as `git_api_req.py Android`, to get results for this particular keyword. ?
+
+## Why this project? ?
+
+This project is a small exercise I conducted, hoping to help others with it. It's a great example of how to use the GitHub API to retrieve useful information. ?
+
+## Let's go! ?
+
+Try it out and see what you can find! You might discover your next favorite repository. ?
+## ? How to use ?
+### Tmux Android  
+```
+ python3 git_api_req.py
+ 
+ python3 git_api_req.py Crawler 
+```
+### Bash  
+```
+ python3 git_api_req.py
+ 
+ python3 git_api_req.py Crawler 
+```
+### Windows  
+```
+ py git_api_req.py
+ 
+ py git_api_req.py Crawler 
+```
--- git_api_req.py
+++ git_api_req.py
@@ -1,32 +1,50 @@
 from colorama import Fore, Style
 import requests, datetime, os, sys
 
-
+uri = 'https://api.github.com/search/repositories'
 now = datetime.datetime.now()
 Y = now.strftime("%Y")
-M = now.strftime("%M")
-# If no argument, then CVE and current year should be used as querry
+M = now.strftime("%m")
+# If no argument, then use CVE- and current year as querry
 arg = sys.argv[1] if len(sys.argv) > 1 else f"cve-{Y}"
-
-uri = 'https://api.github.com/search/repositories'
 params = {'q': arg}
-os.system("clear")
-os.system("cls")
-print(Fore.GREEN + "[+] Start Connection")
-resp = requests.get(uri, params=params)
-js_resp = resp.json()
+def clear_screen():
+  if os.name == "nt":
+    os.system("cls")
+  else:
+    os.system("clear")
+def get_connectio(uri, params):
+  print(Fore.GREEN + "[+] Start Connection")
+  try:
+    resp = requests.get(uri, params=params)
+  except Exception as ex:
+    print("Connection Error to api.github.com")
+    print(Fore.RED + f"[!] Status code: {resp.status_code}")
+  return resp
+def get_data(resp):
+  js_resp = resp.json()
+  print(Fore.YELLOW + "[+] Sort items")
+  total_num = js_resp['total_count']
+  items = js_resp['items']
+  sorted_items = sorted(items, key=lambda x: x['html_url'].split('/')[4].lower())
+  return sorted_items, total_num
+def display_data(sorted_items, total_num, Y, M):
+  for item in sorted_items:
+    y = item['pushed_at'].split('-')[0]
+    m = item['pushed_at'].split('-')[1]
+    last_update = item['pushed_at']
 
-print(Fore.YELLOW + "[+] Sort items")
-total_num = js_resp['total_count']
-items = js_resp['items']
-sorted_items = sorted(items, key=lambda x: x['html_url'].split('/')[4].lower())
-print(Style.RESET_ALL)
-for item in sorted_items: 
-  y = item['pushed_at'].split('-')[0]
-  m = item['pushed_at'].split('-')[1]
-  last_update = item['pushed_at']
-  
-  if (y == Y ):
-    url = item['html_url']
-    print(f"Last Update: {last_update}\nUrl:\n{url}\n")
-print(Fore.GREEN + f"Total: {total_num}")
+    if (y == Y or m == M ):
+      url = item['html_url']
+      print(f"Last Update: {last_update}\nUrl:\n{url}\n")
+  print(Fore.GREEN + f"Total: {total_num}")
+  print(Style.RESET_ALL)
+
+def main():
+  clear_screen()
+  resp =  get_connectio(uri, params)
+  sorted_items, total_num = get_data(resp=resp)
+  display_data(sorted_items, total_num, Y, M)
+
+if __name__ == "__main__":
+  main()
