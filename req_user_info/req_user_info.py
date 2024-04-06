de91c6037af2f12a43e538d54c2d3633da566fad
jocker2410 <jocker2410@gmail.com>
2024-04-06 15:10:49 +0200

    update req_user_info

--- req_user_info/req_user_info.py
+++ req_user_info/req_user_info.py
@@ -12,9 +12,9 @@ print(Fore.GREEN + f"User:\t\t\t{data['login']}\n",
       Fore.YELLOW + f"Repos:\t\t\t{data['public_repos']}\n",
       Fore.BLUE + f"Follow:\t\t\t{data['followers']}\n",
       Fore.LIGHTBLUE_EX + f"Followed:\t\t{data['following']}")
-print(Fore.LIGHTMAGENTA_EX + 30 * "#")
 
 # get contacts
+# https://api.github.com/users/jocker2410/followers
 follower_link = f"{url}/followers"
 follower_resp = requests.get(follower_link)
 follower_data = follower_resp.json()
@@ -22,6 +22,7 @@ print(Fore.CYAN +"\nFollowers:")
 for follow in follower_data:
     print(Fore.GREEN + follow['login'])
 
+# https://api.github.com/users/jocker2410/following
 folowing_link = f"{url}/following"
 folowing_resp = requests.get(folowing_link)
 folowing_data = folowing_resp.json()
@@ -30,11 +31,23 @@ for folowing in folowing_data:
     print(Fore.GREEN + folowing['login'])
 
 # get repo data
-repo_link = f"\n{url}/repos"
+# https://api.github.com/users/jocker2410/repos
+repo_link = f"{url}/repos"
 repo_resp = requests.get(repo_link)
 repo_data = repo_resp.json()
 
-print(Fore.CYAN + f"Repo Urls:")
+print(Fore.CYAN + f"\nRepo Urls:")
 for repo in repo_data:
     print(Fore.GREEN + repo['clone_url'])
-
+print("")
+# get Commits
+for repo in repo_data:
+    repo_name = repo['name']
+    commit_url = f"{url.replace('users', 'repos')}{repo['name']}/commits"
+    commit_data = requests.get(commit_url).json()
+    print(f"Commits for {repo['name']}\n")
+    for commit in commit_data:
+        print(f"Committer:\t{commit['commit']['committer']['name']}",
+              f"E-Mail:\t{commit['commit']['committer']['email']}",
+              f"Update date:\t{commit['commit']['committer']['date']}",
+              f"Message:\t{commit['commit']['committer']['message']}")
